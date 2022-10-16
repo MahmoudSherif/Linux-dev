7d440a9f1f41d6b254241a74aeda05cbd77cd7a8
Mahmoud Sherif <mahmoudsherifm94@gmail.com>
2022-10-16 21:34:39 +0200

    FIrst part of the simple Shell

---
+++ mysimpleShell/mysimpleShell.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+int main(int argc, char** argv )
+{
+
+    int my_pid = getpid();
+    printf("my pid is %d\n",my_pid);
+    while (1)
+    {
+        char buf[100];
+        char args[10][10]; 
+        char* myargv[10];
+        char* myargc[10];
+
+        fputs("ana gahez ya basha >\n",stdout);
+        fgets(buf,100,stdin);
+        
+        int len = strlen(buf);
+
+        buf[len -1 ] = 0;
+        int i=0,j=0,k=0;
+
+        for(;i<len;i++)
+        {
+            if(buf[i]==' ')
+            {
+                args[j][k]=0;
+                myargv[j] = args[j];
+                j++;
+                k=0;
+            }else
+            {
+                args[j][k]=buf[i];
+                k++;
+            }
+        }
+        args[j][k]=0;
+        myargv[j] = args[j];
+        j++;
+        myargv[j] = 0;
+
+        int ret_pid = fork();
+        
+        if(ret_pid < 0)
+        {
+            fputs("ERROR HAPPENED", stdout);
+            /* TODO: ERROR CHECKING */
+
+        }else if (ret_pid > 0)
+        {
+            printf("this is the parent and my pid is %d and my child pid is %d\n",getpid(),ret_pid);
+            int status;
+            wait(&status);
+        }else if (ret_pid == 0)
+        {
+	    printf("The is the child - my id id %d and my parent id is %d\n",getpid(),getppid());
+
+            sleep(1);
+
+            execvp(myargv[0],myargv);
+
+        }
+    }
+        
+    return 0;
+
+}
